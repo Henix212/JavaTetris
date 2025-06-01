aedbb2800b4d7be4cb6244500979417e27b4f315
Henix212 <hugogpdealmeida@gmail.com>
2025-06-01 23:28:01 +0200

    Add test

--- src/test/java/test/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
+++ src/test/java/test/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
@@ -7,6 +7,7 @@ import src.fr.eseo.e3.poo.projet.blox.modele.Coordonnees;
 import src.fr.eseo.e3.poo.projet.blox.modele.Couleur;
 import src.fr.eseo.e3.poo.projet.blox.modele.Puits;
 import src.fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.ITetromino;
+import src.fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos.OTetromino;
 import src.fr.eseo.e3.poo.projet.blox.vue.VuePuits;
 
 import java.awt.event.KeyEvent;
@@ -28,16 +29,6 @@ public class PieceRotationTest {
     }
 
     @Test
-    public void testMouseClickedLeftButton() {
-        puits.setPieceSuivante(null);
-        puits.setPieceSuivante(null);
-        MouseEvent leftClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
-                0, 10, 10, 1, false, MouseEvent.BUTTON1);
-
-        assertDoesNotThrow(() -> pieceRotation.mouseClicked(leftClick));
-    }
-
-    @Test
     public void testMouseClickedRightButton() {
         puits.setPieceSuivante(new ITetromino(new Coordonnees(0, 0), Couleur.BLEU));
         puits.setPieceSuivante(new ITetromino(new Coordonnees(0, 0), Couleur.BLEU));
@@ -48,14 +39,27 @@ public class PieceRotationTest {
     }
 
     @Test
-    public void testKeyPressedUpArrow() {
-        puits.setPieceSuivante(new ITetromino(new Coordonnees(0, 0), Couleur.BLEU));
-        puits.setPieceSuivante(new ITetromino(new Coordonnees(0, 0), Couleur.BLEU));
+    public void testMouseClickedLeftButtonWithPiece() {
+        puits.setPieceSuivante(new ITetromino(new Coordonnees(4, 1), Couleur.BLEU));
+        MouseEvent leftClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
+                0, 10, 10, 1, false, MouseEvent.BUTTON1);
+        assertDoesNotThrow(() -> pieceRotation.mouseClicked(leftClick));
+    }
 
-        KeyEvent keyEvent = new KeyEvent(vuePuits, KeyEvent.KEY_PRESSED, System.currentTimeMillis(),
-                0, KeyEvent.VK_UP, KeyEvent.CHAR_UNDEFINED);
+    @Test
+    public void testMouseClickedRightButtonWithPiece() {
+        puits.setPieceSuivante(new ITetromino(new Coordonnees(4, 1), Couleur.BLEU));
+        MouseEvent rightClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
+                0, 10, 10, 1, false, MouseEvent.BUTTON3);
+        assertDoesNotThrow(() -> pieceRotation.mouseClicked(rightClick));
+    }
 
-        assertDoesNotThrow(() -> pieceRotation.keyPressed(keyEvent));
+    @Test
+    public void testMouseClickedLeftButtonWithOTetromino() {
+        puits.setPieceSuivante(new OTetromino(new Coordonnees(4, 1), Couleur.VERT));
+        MouseEvent leftClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
+                0, 10, 10, 1, false, MouseEvent.BUTTON1);
+        assertDoesNotThrow(() -> pieceRotation.mouseClicked(leftClick));
     }
 
     @Test
@@ -63,6 +67,38 @@ public class PieceRotationTest {
         KeyEvent keyEvent = new KeyEvent(vuePuits, KeyEvent.KEY_PRESSED, System.currentTimeMillis(),
                 0, KeyEvent.VK_LEFT, KeyEvent.CHAR_UNDEFINED);
 
+        assertDoesNotThrow(() -> pieceRotation.keyPressed(keyEvent));
+    }
+
+    @Test
+    public void testKeyPressedUpArrowWithPiece() {
+        puits.setPieceSuivante(new ITetromino(new Coordonnees(4, 1), Couleur.JAUNE));
+        KeyEvent keyEvent = new KeyEvent(vuePuits, KeyEvent.KEY_PRESSED, System.currentTimeMillis(),
+                0, KeyEvent.VK_UP, KeyEvent.CHAR_UNDEFINED);
+        assertDoesNotThrow(() -> pieceRotation.keyPressed(keyEvent));
+    }
+
+    @Test
+    public void testMouseClickedLeftButtonWithoutPiece() {
+        // No piece in the puits
+        MouseEvent leftClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
+                0, 10, 10, 1, false, MouseEvent.BUTTON1);
+        assertDoesNotThrow(() -> pieceRotation.mouseClicked(leftClick));
+    }
+
+    @Test
+    public void testMouseClickedRightButtonWithoutPiece() {
+        // No piece in the puits
+        MouseEvent rightClick = new MouseEvent(vuePuits, MouseEvent.MOUSE_CLICKED, System.currentTimeMillis(),
+                0, 10, 10, 1, false, MouseEvent.BUTTON3);
+        assertDoesNotThrow(() -> pieceRotation.mouseClicked(rightClick));
+    }
+
+    @Test
+    public void testKeyPressedUpArrowWithoutPiece() {
+        // No piece in the puits
+        KeyEvent keyEvent = new KeyEvent(vuePuits, KeyEvent.KEY_PRESSED, System.currentTimeMillis(),
+                0, KeyEvent.VK_UP, KeyEvent.CHAR_UNDEFINED);
         assertDoesNotThrow(() -> pieceRotation.keyPressed(keyEvent));
     }
 }
